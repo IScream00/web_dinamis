733370b908430eb95139f426585e879ec7ebcf0a
IScream00 <73802193+IScream00@users.noreply.github.com>
2020-11-29 19:56:17 +0700

    Delete Fungsi.php

--- Fungsi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function perkenalan(){
-        echo "Salam sejahtera<br>";
-        echo "Selamat datang di web kami<br>";
-        echo "Jangan lupa komen, dan share web ini<br>";
-
-    }
-    perkenalan();
-?>
